5a04924d510cb63541114867ff32e0b90b1f770d
Charles-Louis DUTEL <cldutel51@gmail.com>
2025-12-16 14:28:23 +0100

    modif gestion online/offline websocket pour les connexions, reste à gérer 'on close'

--- services/gateway/websocketHandler/websocketHandler.js
+++ services/gateway/websocketHandler/websocketHandler.js
@@ -1,33 +1,74 @@
-
+const connectionsIndex = new Map();
+const sessionsByUser = new Map();
 const userPresence = new Map();
-const connectionsId = new Map();
 
-const userId = {
-	connection_count: 0,
-	status: "offline",
-	last_seen_at: null,
-	connection: []
+let connectId = 0;
+
+/* struct connectionsIndex :
+{
+	userId,
+	connectionId,
+	ip
 }
+*/
+
+/* struct sessionsByUser :
+{
+	socketSet: Set(),
+	offlineTimer,
+	connectedSince
+}
+*/
+
+/* struct userPresence :
+{
+	status,
+	lastSeenAt,
+	activeSince
+}
+*/
 
 export const websocketHandler = async function (socket, req) {
 	try {
 		await req.jwtVerify();
+		const date = new Date().toISOString();
+		
+		connectionsIndex.set(socket, {
+			userId: req.user.id,
+			connectionId: connectId++,
+			ip: req.ip
+		});
 
-		const currentUser = userPresence.get(req.user.id);
-		if (currentUser)
+		let currentUserSession = sessionsByUser.get(req.user.id);
+		if (!currentUserSession)
 		{
-			currentUser.connection_count++;
-			currentUser.connection.push(socket);
+			currentUserSession = {
+				socketSet: new Set(),
+				offlineTimer: null,
+				connectedSince: date,
+			};
+			sessionsByUser.set(req.user.id, currentUserSession);
 		}
-		else
+		currentUserSession.socketSet.add(socket);
+
+		const currentUserPresence = userPresence.get(req.user.id);
+		let isOnlineNow = currentUserSession.socketSet.size > 0;
+		if (!currentUserPresence)
 		{
 			userPresence.set(req.user.id, {
-				connection_count: 1,
 				status: "online",
-				last_seen_at: null,
-				connection: [socket]
+				lastSeenAt: null,
+				activeSince: date
 			});
 		}
+		else if (currentUserPresence.status === "offline" && isOnlineNow === true)
+		{
+			currentUserPresence.status = "online";
+			currentUserPresence.lastSeenAt = null;
+			currentUserPresence.activeSince = date;
+		}
+
+
 		
 		socket.addEventListener("message", (event) => {
 			console.log(`\nwebsocketHandler Message: ${event.data}\n`);

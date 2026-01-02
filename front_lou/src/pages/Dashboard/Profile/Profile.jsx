b4441750b73b35f7c4c1c5331899ee608e5110b6
Lou Brtt <briottetlou@gmail.com>
2026-01-02 23:21:21 +0300

    build (frontend/profilepage): adding useEffect on the profile page to link the back with the frontend

--- front_lou/src/pages/Dashboard/Profile/Profile.jsx
+++ front_lou/src/pages/Dashboard/Profile/Profile.jsx
@@ -1,8 +1,45 @@
 import '../../../styles/App.css'
 import { LogTitle, Footer, Background, HeaderBar, LeftMenu} from '../../../components'
 import './Profile.css' 
+import { useState, useEffect } from 'react'
+import { profilepicture } from '../../../assets'
 
 function Profile() {
+  const [userData, setUserData] = useState ({
+      id: '',
+      username: '',
+      email: '',
+      avatar_path: '',
+      createdAt: ''
+  });
+  useEffect(() => {
+    const fetchProfile = async () => {
+    try {
+            const reponse = await fetch('http://localhost:5000/api/v1/auth/me', {
+                method : 'GET',
+                headers :{
+                    'Content-Type': 'application/json'
+                },
+                credentials: 'include'
+            })
+            if (reponse.ok){
+                const data = await reponse.json();
+                console.log(data.avatar_path);
+                setUserData(data);
+            }
+        }
+        catch (err){
+            console.error('Erreur :', err)
+        }
+      }
+  fetchProfile()
+     }, []);
+     if (!userData.avatar_path){
+      setUserData({
+        ...userData,
+        avatar_path : profilepicture
+      })
+     }
   return (
     <>
       <Background>
@@ -10,7 +47,9 @@ function Profile() {
           <HeaderBar/>
           <LogTitle text="Mon profil"/>
           <div className='profile-wrapper'>
-            <div className='profile-picture'/>
+            <div className='profile-picture'>
+              <img src={userData.avatar_path} className='profilepic'/>
+            </div>
               <div className='personal-infos'>
                 Mes informations personnelles
               </div>

4bc36e5920fd753ff413230bd422c0e7c4016b60
Lou Brtt <briottetlou@gmail.com>
2026-01-15 15:35:32 +0100

    feat(search-bar): implementing functionnal searchbar for all frontend features, its possible to access 9 searchable page but filtering is not implemented yet

---
+++ services/frontend/src/components/HeaderBar/SearchConfig.js
@@ -0,0 +1,90 @@
+import Dashboard from "../../pages/Dashboard/Dashboard";
+
+export const searchablePages = [
+    {
+        id: 'dashboard',
+        title: 'Dashboard',
+        description: "Page d'accueil",
+        path: '/dashboard',
+        icon: '🏠',
+        keywords: [ 'home', 'accueil', 'dashboard' ]
+    },
+    {
+        id: 'profile',
+        title: 'Profil',
+        description: 'Voir et modifier mon profil',
+        path: '/dashboard/profile',
+        icon: '👤',
+        keywords: [ 'moi', 'me', 'profile', 'profil', 'compte', 'account' ]
+    },
+    {
+        id: 'settings',
+        title: 'Réglages',
+        description: 'Voir et modifier mes réglages',
+        path: '/dashboard/settings',
+        icon: '⚙️',
+        keywords: [ 'settings', 'reglages']
+    },
+    {
+        id: 'activity',
+        title: 'Mes activités',
+        description: 'Consulter mes activités',
+        path: '/dashboard/activity',
+        icon: '📊',
+        keywords: [ 'activity' , 'agenda' ]
+    },
+    {
+        id: 'achievement',
+        title: 'Mes réussites',
+        description: 'Consulter mes achievments',
+        path: '/dashboard/achievement',
+        icon: '🏆',
+        keywords: [ 'goals' , 'achievement' , 'badges', 'reussites']
+    },
+    {
+        id: 'conversations',
+        title: 'Messageries',
+        description: 'Ouvrir le fil des conversations',
+        path: "/dashboard/conversations" ,
+        icon: '💬',
+        keywords: [ 'messages', 'message', 'conversations' ]
+    },
+    {
+        id: 'ressources',
+        title: 'Mes ressources',
+        description: 'Consulter ma documentation',
+        path: "/dashboard/ressources",
+        icon: '📚',
+        keywords: [ 'docs', 'informations', 'documentations' ]
+    },
+    {
+        id: 'export',
+        title: 'Exporter mes données' ,
+        description: 'Sauvegarder et consulter mes données personnelles',
+        path: "/dashboard/exportdata",
+        icon: '📤' ,
+        keywords: [ 'export' , 'save', 'data' ]
+    },
+    {
+        id: 'about',
+        title: 'A propos',
+        description: 'Découvrez qui se cache derriere cette masterclass',
+        path: "/dashboard/about",
+        icon: '📌',
+        keywords: [ 'about' , 'us', 'propos']
+    }
+        // {
+    //     id: ,
+    //     title: ,
+    //     description: ,
+    //     path: ,
+    //     icon: ,
+    //     keywords: [ , ]
+    // },
+]
+
+export function searchPages(search){
+    if (!search)
+        return (searchablePages.slice(0, 10));
+    return (searchablePages.slice(0, 10));
+}

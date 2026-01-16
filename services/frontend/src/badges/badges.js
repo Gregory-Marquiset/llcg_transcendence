78e9977e0456cf8c731c283ba2fafadb165f4ff8
Lou Brtt <briottetlou@gmail.com>
2026-01-16 18:47:56 +0100

    build(feat/badges): uploading badges and creating tab with all assignation criterias specified

---
+++ services/frontend/src/badges/badges.js
@@ -0,0 +1,73 @@
+export const badges = [
+    {
+        name : 'Trophee',
+        category : 'Ranking',
+        level_1 : 'Under top 10',
+        nb_1 : 10,
+        level_2 : 'Under top 3',
+        nb_2 : 3,
+        level_3 : 'Top 1',
+        nb_3 : 1,
+        path : './trophee.png'
+    },
+    {
+        name : 'Croissance',
+        category : 'Productivity',
+        level_1 : '10 tasks completed',
+        nb_1 : 10,
+        level_2 : '100 tasks completed',
+        nb_2 : 100,
+        level_3 : '500 tasks completed',
+        nb_3 : 500,
+        path : './croissance.png'
+    },
+    {
+        name : 'Like',
+        category : 'Number of friendship',
+        level_1 : '1 friend',
+        nb_1 : 1,
+        level_2 : '10 friends',
+        nb_2 : 10,
+        level_3 : '30 friends',
+        nb_3 : 30,
+        path : './like.png'
+    },
+    {
+        name : 'Medal',
+        category : 'Number of days connected in a row',
+        level_1 : '1 week',
+        nb_1 : 7,
+        level_2 : '1 month',
+        nb_2 : 30,
+        level_3 : '3 months',
+        nb_3 : 90,
+        path : './medal.png'
+    },
+    {
+        name : 'Screen',
+        category : 'LogTime recap in the month (reset every month)',
+        level_1 : '1 hour',
+        nb_1 : 1,
+        level_2 : '30 hours',
+        nb_2 : 30,
+        level_3 : '100 hours',
+        nb_3 : 100,
+        path : './screen.png'
+    },
+    {
+        name : 'Spaceship',
+        category : 'Seniority on the app',
+        level_1 : '1 day',
+        nb_1 : 1,
+        level_2 : '1 month',
+        nb_2 : 30,
+        level_3 : '1 year',
+        nb_3 : 365,
+        path : './spaceship.png'
+    },
+    {
+        name : 'Star',
+        category : 'Admin',
+        path : './star.png'
+    }
+]
